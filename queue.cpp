40b2b5b43cf0848273c2580d4e5bb4896c0312ad
pasyaa <mhmdpsyh@gmail.com>
2026-05-28 21:19:12 +0700

    queue_array[REAR] = num;

--- queue.cpp
+++ queue.cpp
@@ -35,6 +35,11 @@ class Queues{
 
         else{
         // jika rear berada di posisi terakhir array, kembali ke awal array
-        
-
+        if (REAR == max - 1)
+            REAR = 0;
+        else
+            REAR = REAR + 1;
         }
+
+        queue_array[REAR] = num;
+    
