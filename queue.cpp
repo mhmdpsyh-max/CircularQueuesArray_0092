5d2f9f897aa9ad05b29008cfa11785a77b728a27
pasyaa <mhmdpsyh@gmail.com>
2026-05-28 21:27:47 +0700

    jika FRONT <= REAR, iterasi dari FRONT hingga REAR

--- queue.cpp
+++ queue.cpp
@@ -80,5 +80,14 @@ class Queues{
         int REAR_Position = REAR;
 
         // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        // jika FRONT <= REAR, iterasi dari FRONT hingga REAR
         
     }
